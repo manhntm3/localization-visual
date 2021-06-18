eaa64da44b5f9d8c9410d6f0d10b15aa4986122f
Manh Nguyen <manhntm3@gmail.com>
2021-06-19 00:05:51 +0700

    Fix duplicate line

--- visual_dynamic.py
+++ visual_dynamic.py
@@ -7,6 +7,7 @@ import decimal
 import numpy as np 
 from pyproj import Proj, transform
 from matplotlib import animation 
+from matplotlib.lines import Line2D
 
 from scipy.spatial.transform import Rotation as R
 
@@ -17,31 +18,6 @@ def read_csv(csv_filepath):
         for row in csv_reader:
             csv_list.append(row)
     return csv_list
-
-def animation_frame(i):
-    # print(x_coord_st[i], y_coord_st[i])
-    line_gps.set_xdata(x_coord_gps[:i+1])
-    line_gps.set_ydata(y_coord_gps[:i+1])
-
-    ax.set_xlim(x_coord_gps[i]-10, x_coord_gps[i]+10)
-    ax.set_ylim(y_coord_gps[i]-10, y_coord_gps[i]+10)
-    
-    head_x_1, head_y_1, head_x_2, head_y_2 = draw_ego_car(i)
-    line_head_1.set_xdata(head_x_1)
-    line_head_1.set_ydata(head_y_1)
-    line_head_2.set_xdata(head_x_2)
-    line_head_2.set_ydata(head_y_2)
-    # print(x_coord_gps[i], y_coord_gps[i])
-    # print(head_x_1, head_y_1)
-    moving_cum = 0
-
-    while times_gps[i] > times_st[moving_cum]:
-        moving_cum+=1
-
-    line_st.set_xdata(x_coord_st[:moving_cum])
-    line_st.set_ydata(y_coord_st[:moving_cum])
-
-    return line_gps, line_st, line_head_1, line_head_2
 
 def get_corners(pos, rot_mat, ego_size):
     w, l = ego_size
@@ -79,6 +55,36 @@ def draw_ego_car(sample_idx):
             head_y_2.append(y_vals)
     return head_x_1, head_y_1, head_x_2, head_y_2
 
+def animation_frame(i):
+    # print(x_coord_st[i], y_coord_st[i])
+    line_gps.set_xdata(x_coord_gps[:i+1])
+    line_gps.set_ydata(y_coord_gps[:i+1])
+
+    ax.set_xlim(x_coord_gps[i]-10, x_coord_gps[i]+10)
+    ax.set_ylim(y_coord_gps[i]-10, y_coord_gps[i]+10)
+    
+    moving_cum = 0
+
+    while times_gps[i] > times_st[moving_cum]:
+        moving_cum+=1
+    head_x_1, head_y_1, head_x_2, head_y_2 = draw_ego_car(moving_cum-1)
+    ax.get_lines().pop(-1).remove()
+    ax.get_lines().pop(-1).remove()
+    ax.get_lines().pop(-1).remove()
+    ax.get_lines().pop(-1).remove()
+    if i > 0:
+        ax.get_lines().pop(-1).remove()
+        ax.get_lines().pop(-1).remove()
+    line_head_1 = Line2D(head_x_1, head_y_1, color='c')
+    line_head_2 = Line2D(head_x_2, head_y_2, color='g')
+    ax.add_line(line_head_1)
+    ax.add_line(line_head_2)
+
+    line_st.set_xdata(x_coord_st[:moving_cum])
+    line_st.set_ydata(y_coord_st[:moving_cum])
+
+    return line_gps, line_st, line_head_1, line_head_2
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Visualisation')
     parser.add_argument('--input_gps', type=str, required=True, help='gps file')
@@ -114,18 +120,23 @@ if __name__=="__main__":
     ax.grid()
     line_gps, = ax.plot(x_coord_gps[0], y_coord_gps[0], 'r', label="GPS")
     line_st, = ax.plot(x_coord_st[0], y_coord_st[0], 'b', label="State")
-    line_head_1, = ax.plot(x_coord_st[0], y_coord_st[0], 'c')
-    line_head_2, = ax.plot(x_coord_st[0], y_coord_st[0], 'g')
+    # line_head_1 = Line2D([x_coord_st[0]], [y_coord_st[0]], color='c')
+    # line_head_2 = Line2D([x_coord_st[0]], [y_coord_st[0]], color='g')
+    # line_head_1, = ax.plot(x_coord_st[0], y_coord_st[0], 'c')
+    # line_head_2, = ax.plot(x_coord_st[0], y_coord_st[0], 'g')
+    # ax.add_line(line_head_1)
+    # ax.add_line(line_head_2)
     # moving_timestamp = time_st[0]
     # print(times_gps)
     Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
+    writer = Writer(fps=10, metadata=dict(artist='Me'), bitrate=2500)
 
-    line_ani = animation.FuncAnimation(fig, func=animation_frame, frames = np.arange(0,10000,1), interval=20, blit=True)
+    line_ani = animation.FuncAnimation(fig, func=animation_frame, frames = np.arange(0,10000,1), interval=20, blit=False, repeat=False, save_count=200)
     # plt.show()
 
     plt.legend()
-    # line_ani.save('output/lines.mp4', writer=writer)
+    line_ani.save('output/lines.mp4', writer=writer)
+    
     # plt.grid()
     plt.show()
 
